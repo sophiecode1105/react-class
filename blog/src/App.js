4d4a5d70239e1087238c46982fdd1ef890b5f0dc
sophiecode1105 <sophiecode1105@gmail.com>
2021-11-18 22:32:27 +0900

    state

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,6 +5,13 @@ function App() {
 
  let [글제목,글제목변경] = useState(['봄바일기','채영일기','민우일기']);
  let [따봉,따봉변경] = useState(0)
+
+ let [modal,setModal] = useState(false) //on off 스위치다. 모달창을 켜고닫는스위치
+
+ function modalClick(){
+   setModal(!modal)
+ }
+
  function onClick(){
    따봉변경(따봉+1)
  }
@@ -25,19 +32,30 @@ function App() {
         <p>2월 17일 발행</p>
         <hr/>
       </div>
-      <button onClick = { 제목바꾸기 }>버튼</button> 
       <div className = "list">
         <h3>{ 글제목[1] }</h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
       <div className = "list">
-        <h3>{ 글제목[2] }</h3>
+        <h3 onClick = { modalClick }>{ 글제목[2] }</h3>
         <p>2월 17일 발행</p>
         <hr/>
+        <button onClick = { 제목바꾸기 }>버튼</button> 
       </div>
+      {modal === true ? <Modal></Modal> : null}
     </div>
   );
 }
 
+
+function Modal(){
+  return (
+    <div className ="modal">
+          <h2>제목</h2>
+          <p>날짜</p>
+          <p>상세내용</p>
+        </div>
+  ) 
+}
 export default App;
