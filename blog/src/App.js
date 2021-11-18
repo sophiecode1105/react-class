847cfb47b84c55e6213a28b3413ed6387513e734
sophiecode1105 <sophiecode1105@gmail.com>
2021-11-19 00:20:59 +0900

    props연습

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,55 +4,51 @@ import './App.css';
 function App() {
 
  let [글제목,글제목변경] = useState(['봄바일기','채영일기','민우일기']);
- let [따봉,따봉변경] = useState(0)
-
- let [modal,setModal] = useState(false) //on off 스위치다. 모달창을 켜고닫는스위치
-
+ let [따봉,따봉변경] = useState([0,0,0])
+ let [modal,setModal] = useState(false)
+ let [누른제목, 누른제목변경] = useState(0)
  function modalClick(){
    setModal(!modal)
  }
 
- function onClick(){
-   따봉변경(따봉+1)
- }
- function 제목바꾸기(){ // 쌩으로 글제목은 바꾸지못한다. 그러니 바꾸고싶을때 ! 복사를 한다. (하나의 기술) deep copy = spread operator
-   let newArr = [...글제목]
-   newArr.sort()
-   글제목변경(newArr)
- }
-  let posts = '강남고기맛집'
+ function onClick(i){
+    let copyArr = [...따봉]
+    copyArr[i]++
+    따봉변경(copyArr)
+   }
+
+ 
+//  function 제목바꾸기(){ // 쌩으로 글제목은 바꾸지못한다. 그러니 바꾸고싶을때 ! 복사를 한다. (하나의 기술) deep copy = spread operator
+//    let newArr = [...글제목]
+//    newArr.sort()
+//    글제목변경(newArr)
+//  }
 
   return (
     <div className="App">
     <div className="black-nav">
       <div>Sophie Blog</div>
-    </div>
-      <div className = "list">
-        <h3>{ 글제목[0] } <span onClick={ onClick }>👍🏻</span>{따봉}</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className = "list">
-        <h3>{ 글제목[1] }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className = "list">
-        <h3 onClick = { modalClick }>{ 글제목[2] }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-        <button onClick = { 제목바꾸기 }>버튼</button> 
-      </div>
-      {modal === true ? <Modal></Modal> : null}
+    </div> 
+      {
+        글제목.map((el,i)=>{
+         return  <div className = "list">
+         <h3 onClick = {()=>{누른제목변경(i)}}>{ el } <span onClick={()=>onClick(i)}>👍🏻</span>{따봉[i]}</h3>
+         <p>2월 17일 발행</p>
+         <hr/>
+       </div>
+        })
+      }
+      <button onClick = {modalClick}>열고닫기</button>
+      {modal ? <Modal 글제목={글제목} 누른제목={누른제목} /> : null }
     </div>
   );
 }
 
 
-function Modal(){
+function Modal(props){
   return (
     <div className ="modal">
-          <h2>제목</h2>
+          <h2>{props.글제목[props.누른제목]}</h2>
           <p>날짜</p>
           <p>상세내용</p>
         </div>
